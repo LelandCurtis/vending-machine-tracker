3ba6defb5aaacc0a33444110a06a887c93e758a5
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-02 17:21:49 -0500

    Fix typo in many_many association tests.

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -2,5 +2,5 @@ require 'rails_helper'
 
 RSpec.describe Snack, type: :model do
   it {should have_many :machine_snacks}
-  it {should have_many(:machines), through(:machine_snacks)}
+  it {should have_many(:machines).through(:machine_snacks)}
 end
