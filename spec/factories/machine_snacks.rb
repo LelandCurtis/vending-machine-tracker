601a480b5227c25b7558304f5d78c18f72058fac
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-02 17:07:10 -0500

    Create machine_snack model join table for many-many relationship.

---
+++ spec/factories/machine_snacks.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :machine_snack do
+    snack { nil }
+    machine { nil }
+  end
+end
--- spec/models/machine_spec.rb
+++ spec/models/machine_spec.rb
@@ -6,6 +6,5 @@ RSpec.describe Machine, type: :model do
     it { should belong_to :owner }
     it { should have_many :machine_snacks }
     it { should have_many(:snacks), through(:machine_snacks)}
-
   end
 end
