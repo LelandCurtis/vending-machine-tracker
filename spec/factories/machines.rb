73a2bb27869d5f3e93a328004d880d69fb5bdcd9
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-02 17:30:04 -0500

    Create machine_with_snacks factory.

--- spec/factories/machines.rb
+++ spec/factories/machines.rb
@@ -2,5 +2,17 @@ FactoryBot.define do
   factory :machine do
     sequence(:location) {|l| "Default Location #{l}"}
     association :owner, factory: :owner
+
+    factory :machine_with_snacks do
+      transient do
+        snack_count {2}
+      end
+      after(:create) do |machine, evaluator|
+        evaluator.snack_count.times do
+          snack = create(:snack)
+          machine_snack = create(:machine_snack, machine: machine, snack: snack)
+        end
+      end
+    end
   end
 end
