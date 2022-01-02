9a07514fc003f41e7798b3c777f319d06654ef1a
Leland Curtis <lelandcurtis88@gmail.com>
2022-01-02 16:46:55 -0500

    Write user story 1 test.

---
+++ spec/features/machine/show_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe 'machine show page' do
+  it "shows all of the snacks associated with the vening machine" do
+    machine = create(:machine_with_snacks, snack_count: 3)
+
+    visit "/machines/#{machine.id}"
+
+    expect(page).to have_content("Default Name 1")
+    expect(page).to have_content("Default Name 2")
+    expect(page).to have_content("Default Name 3")
+  end
+
+  it "shows all of the prices associated with each snack rounded to 2 digits" do
+    machine = create(:machine_with_snacks, snack_count: 3)
+    visit "/machines/#{machine.id}"
+
+    expect(page).to have_content("$1.11")
+    expect(page).to have_content("$2.11")
+    expect(page).to have_content("$3.11")
+  end
+end
